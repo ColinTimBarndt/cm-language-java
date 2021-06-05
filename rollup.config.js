44b16cee65770bfc35e08002d5eb5b0bc1b69a06
ColinTimBarndt <colin.barndt@gmail.com>
2021-06-05 23:46:25 +0200

    Base setup

---
+++ rollup.config.js
@@ -0,0 +1,20 @@
+import typescript from "@rollup/plugin-typescript";
+import { nodeResolve } from "@rollup/plugin-node-resolve";
+
+export default {
+	input: "src/index.ts",
+	output: [
+		{
+			file: "dist/index.js",
+			format: "es"
+		},
+		{
+			file: "dist/index.cjs",
+			format: "cjs"
+		}
+	],
+	plugins: [
+		typescript(),
+		nodeResolve()
+	]
+}
---
+++ src/index.ts
@@ -0,0 +1,2 @@
+type A = "test";
+console.log("Tets");
